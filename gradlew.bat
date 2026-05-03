0e0167488dcd32f8942f8ef7e7c2b82f80d493cb
WorldSpriggan1 <worldspriggan8@gmail.com>
2026-05-02 23:42:55 -0400

    Enhance gradlew.bat with error checks
    Added validation for APP_HOME and gradle-wrapper.jar existence.

--- gradlew.bat
+++ gradlew.bat
@@ -13,6 +13,14 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
+@rem Validate APP_HOME directory exists
+if not exist "%APP_HOME%" (
+    echo.
+    echo ERROR: APP_HOME directory not found: %APP_HOME%
+    echo.
+    goto fail
+)
+
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
@@ -28,12 +36,13 @@ echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PA
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
+echo.
 
 goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set JAVA_EXE=%JAVA_HOME%\bin\java.exe
 
 if exist "%JAVA_EXE%" goto init
 
@@ -42,6 +51,9 @@ echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
+echo.
+echo Current JAVA_HOME: %JAVA_HOME%
+echo.
 
 goto fail
 
@@ -65,6 +77,15 @@ set CMD_LINE_ARGS=%*
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
+@rem Validate gradle-wrapper.jar exists
+if not exist "%CLASSPATH%" (
+    echo.
+    echo ERROR: gradle-wrapper.jar not found at:
+    echo %CLASSPATH%
+    echo.
+    goto fail
+)
+
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
@@ -75,7 +96,7 @@ if "%ERRORLEVEL%"=="0" goto mainEnd
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+if not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
